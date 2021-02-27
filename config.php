d22b74029221429180a550395da867c781290876
mahmoudmagdy459 <79756050+mahmoudmagdy459@users.noreply.github.com>
2021-02-27 18:36:29 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,9 @@ define("HOSTNAME",''); //NO-IP Domain - hostname.ddns.net
 define("HOSTPORT",''); //NO-IP Port   - 8080
 define("IS_SECURE", false);
 define("API_VERSION", 'v1');
+
+  $username = '';  //Your username
+  $password = ''; //Your Password
+  $APP_USERNAME = '';
+  $APP_PASSWORD = '';
 ?>
